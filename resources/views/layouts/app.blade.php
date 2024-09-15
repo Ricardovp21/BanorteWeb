572da7d801a957713562e6d994fb6a24de043cd3
Juan Gerardo Alcantar Torres <53959987+GDesasT@users.noreply.github.com>
2024-09-14 21:48:06 -0600

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,11 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html lang="es">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <title>Banorte</title>
 </head>
 <body class="">
     @include('layouts.navbar')
